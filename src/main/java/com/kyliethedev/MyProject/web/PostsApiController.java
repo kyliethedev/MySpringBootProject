292c8b4e4860ec0d3d092af641ebd14cfe21882b
KylieM <kyliethedev@gmail.com>
2021-08-09 22:37:02 +0900

    Bug Fix(jdbc URL, @ResponseBody)

--- src/main/java/com/kyliethedev/MyProject/web/PostsApiController.java
+++ src/main/java/com/kyliethedev/MyProject/web/PostsApiController.java
@@ -4,9 +4,7 @@ import com.kyliethedev.MyProject.service.PostsService;
 import com.kyliethedev.MyProject.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Controller
@@ -15,6 +13,7 @@ public class PostsApiController {
     private final PostsService postsService;
 
     @PostMapping("/api/v1/posts")
+    @ResponseBody
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
