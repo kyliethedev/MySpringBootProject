3d06d629d012fff650d19c575db11ab0ab2c2c4a
KylieM <kyliethedev@gmail.com>
2021-08-16 15:10:26 +0900

    oauth 네이버 로그인추가

--- src/main/java/com/kyliethedev/MyProject/web/IndexController.java
+++ src/main/java/com/kyliethedev/MyProject/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kyliethedev.MyProject.web;
 
+import com.kyliethedev.MyProject.config.auth.LoginUser;
 import com.kyliethedev.MyProject.config.auth.dto.SessionUser;
 import com.kyliethedev.MyProject.service.PostsService;
 import com.kyliethedev.MyProject.web.dto.PostsResponseDto;
@@ -20,10 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if (user != null) {
             model.addAttribute("userName", user.getName());
