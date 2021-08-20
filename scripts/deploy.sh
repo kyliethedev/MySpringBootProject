3e77c84e27483e6492009eb21e8c97e9a5a21e19
KylieM <kyliethedev@gmail.com>
2021-08-20 14:29:01 +0900

    AWS 자동배포를 위한 파일 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,35 +1,39 @@
-#! /bin/bash
+#!/bin/bash
 
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=MySpringBootProject
 
-echo "> Copy Build files"
-cp $REPOSITORY/zip/*.jar $REPOSITORY/.file
+echo "> Build 파일 복사"
 
-echo "> Check pid"
-CURRENT_PID=$(pgrep -fl MySpringBootProject || grep jar | awk '{print $1}')
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
-echo "> current pid : $CURRENT_PID"
+echo "> 현재 구동중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl MySpringBootProject | grep jar | awk '{print $1}')
+
+echo "현재 구동중인 어플리케이션 pid: $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
-  echo "> No Running Application"
+    echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -15 $CURRENT_PID"
-  kill -15 $CURRENT_PID
-  sleep 5
+    echo "> kill -15 $CURRENT_PID"
+    kill -15 $CURRENT_PID
+    sleep 5
 fi
 
-echo " Deploy New Application"
+echo "> 새 어플리케이션 배포"
 
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
-echo "> Jar Name : $JAR_NAME"
+echo "> JAR Name: $JAR_NAME"
 
-echo "> Allow Permission to $JAR_NAME"
+echo "> $JAR_NAME 에 실행권한 추가"
+
 chmod +x $JAR_NAME
 
-echo "> Run $JAR_NAME"
+echo "> $JAR_NAME 실행"
+
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec-2user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=real \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
